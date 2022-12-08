50058057fc941e3171aae67b51ab3bd413bff355
Wesley <w.damiao15@gmail.com>
2022-12-08 16:43:37 -0300

    add animation

--- src/components/ClickedPoints.js
+++ src/components/ClickedPoints.js
@@ -1,15 +1,15 @@
 export default function ClickedPoint({clicked, bg}){
 
     return (
-        <div style={{
+        <div className="clickedPoints" style={{
             left: clicked.clientX - 10,
             top: clicked.clientY -10,
             position: "absolute",
             backgroundImage: `url(${bg.src})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
-            width: "35px",
-            height: "35px",
+            width: "65px",
+            height: "65px",
             borderRadius: "50%",
             display:"block",
         }}></div>
