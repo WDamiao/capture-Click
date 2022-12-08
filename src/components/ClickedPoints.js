eacc3caa8ed851fc69bee1c0649913de660180e7
Wesley <w.damiao15@gmail.com>
2022-12-08 17:00:50 -0300

    adjust clickedPoints position

--- src/components/ClickedPoints.js
+++ src/components/ClickedPoints.js
@@ -2,14 +2,14 @@ export default function ClickedPoint({clicked, bg}){
 
     return (
         <div className="clickedPoints" style={{
-            left: clicked.clientX - 10,
-            top: clicked.clientY -10,
+            left: clicked.clientX - 15,
+            top: clicked.clientY -20,
             position: "absolute",
             backgroundImage: `url(${bg.src})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
-            width: "65px",
-            height: "65px",
+            width: "35px",
+            height: "35px",
             borderRadius: "50%",
             display:"block",
         }}></div>
